7f22d62597e676b5b6b04a39bc652f0a22ac5b9d
cheongsulee <leecheongsu@pharmcadd.com>
2022-07-25 16:42:30 +0900

    :art: Express.js Docs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const fs = require('fs')
+const router = express.Router()
+const Query = require('pg').Query
+
+// 실시간 처리가 필요할 때 유용함.
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -50,6 +54,77 @@ router.post('/upload/multi', async (req, res) => {
         res.status(500).send(e)
     }
 })
+
+//route params : /:id
+//query string : req.query.name
+
+router.get('/read', async (req, res) => {
+    try {
+        fs.readFile(`./uploads/${req.query.file}`, 'utf-8', function(err, data) {
+            if(err != null) {
+               res.status(500).send(err)
+            }
+            //TODO : parsing
+        })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+//method에 국한 되지 않고 받음.
+router.all('/secret', async (req, res) => {
+
+}, async (req, res) => {
+    // 두개의 콜백 함수 실행 가능.
+})
+
+const app = express()
+
+// 하나의 path에 여러개의 method를 chaining 할 수 있음.
+app.route('/triple')
+    .get('/get', async (req, res, next) => {
+        // next 는 콜백인수
+    })
+    .post()
+
+
+const requestTime = function(req, res, next) {
+    req.requestTime = Date.now()
+    next()
+}
+
+router.use(requestTime)
+
+router.get('/time', async (req, res) => {
+    res.send(req.requestTime)
+    // 미들웨어 함수를 등록후 사용할 수 있다.
+})
+
+router.use(async (req, res, next) => {
+    //마운트 경로가 없을 경우 request가 있을때마다 함수 실행.
+})
+
+
+//어플리케이션 레벨 미들웨어
+router.get('/user', async (req, res, next) => {
+    console.log(req.originalUrl)
+    next()
+}, async (req, res, next) => {
+    console.log(req.method)
+    next()
+})
+
+
+//라우터 레벨 미들웨어
+router.get('/user/:id', async(req, res, next) => {
+    if(req.params.id === 0) next('route')
+    next()
+}, async (req, res, next) => {
+    res.render('regular page') // id가 0이 아니고 정상페이지 동작할때
+})
+
+
+
 
 
 module.exports = router
